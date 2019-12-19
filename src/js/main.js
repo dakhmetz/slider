81ef868404dd88cda03981f3b9c80485c3814b88
Dastan Akhmetzhanov <58333215+dakhmetz@users.noreply.github.com>
2019-12-19 16:43:34 +0500

    fix: fixed all sized as per indicated in design

--- src/js/main.js
+++ src/js/main.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
 
-  next.css('left', prev.width() + 10 + bullets.width() + 10);
-  bullets.css('left', prev.width() + 10);
+  next.css('left', prev.width() + bullets.width() + 43);
+  bullets.css('left', prev.width() + 30);
 
 });
