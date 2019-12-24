79affb8db766669f3d76b497ad871aad522ccea3
Dastan Akhmetzhanov <58333215+dakhmetz@users.noreply.github.com>
2019-12-24 21:07:07 +0500

    initialized basic environment

--- src/js/main.js
+++ src/js/main.js
@@ -68,23 +68,5 @@ $(document).ready(function () {
     }, delay);
   });
 
-  var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  var next = $('.swiper-button-next');
-  var prev = $('.swiper-button-prev');
-  var bullets = $('.swiper-pagination');
-
-  next.css('left', prev.width() + bullets.width() + 43);
-  bullets.css('left', prev.width() + 30);
 
 });
