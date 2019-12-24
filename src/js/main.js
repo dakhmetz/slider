6d299e70afe41ec5991e8debf72bb0c7428ff5d9
Dastan Akhmetzhanov <58333215+dakhmetz@users.noreply.github.com>
2019-12-24 23:24:13 +0500

    feat: add slider

--- src/js/main.js
+++ src/js/main.js
@@ -68,5 +68,24 @@ $(document).ready(function () {
     }, delay);
   });
 
+   //initialize swiper when document ready
+   var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10)
 
 });
